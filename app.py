401559f7f429e40c359f9e9d3cf68210d6dac5f9
Christoffer Lilja <christoffer.lilja@zilux.se>
2026-02-19 08:42:15 +0100

    feat: Restore favicon.ico
    This commit restores the  file and its corresponding route in  as requested by the user.
    
    - The  file has been re-created in the  directory.
    - The  definition has been added back to .

--- app.py
+++ app.py
@@ -167,6 +167,11 @@ def product_proxy(barcode):
 def index():
     return render_template("index.html")
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
 @app.route("/manifest.json")
 def serve_manifest():
     return app.send_static_file('manifest.json')
